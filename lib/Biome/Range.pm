10cb0caa2cce9905cba02461989f43c5bf95cf97
Chris Fields <cjfields@bioperl.org>
2009-10-17 10:05:30 -0500

    * simple Range class * gear up for features now that Locations mostly work

---
+++ lib/Biome/Range.pm
@@ -0,0 +1,190 @@
+package Biome::Range;
+
+use Biome;
+
+with 'Biome::Role::Rangeable';
+
+no Biome;
+
+__PACKAGE__->meta->make_immutable();
+
+1;
+
+__END__
+
+=head2 start
+
+  Title   : start
+  Usage   : $start = $range->start();
+  Function: get/set the start of this range
+  Returns : the start of this range
+  Args    : optionally allows the start to be set
+            using $range->start($start)
+
+=cut
+
+=head2 end
+
+  Title   : end
+  Usage   : $end = $range->end();
+  Function: get/set the end of this range
+  Returns : the end of this range
+  Args    : optionally allows the end to be set
+            using $range->end($end)
+
+=cut
+
+=head2 length
+
+  Title   : length
+  Usage   : $length = $range->length();
+  Function: get/set the length of this range
+  Returns : the length of this range
+  Args    : optionally allows the length to be set
+             using $range->length($length)
+
+=cut
+
+=head2 strand
+
+  Title   : strand
+  Usage   : $strand = $range->strand();
+  Function: get/set the strand of this range
+  Returns : the strandedness (-1, 0, +1)
+  Args    : optionally allows the strand to be set
+            using $range->strand($strand)
+
+=cut
+
+=head1 Boolean Methods
+
+These methods return true or false. They throw an error if start and
+end are not defined.
+
+  $range->overlaps($otherRange) && print "Ranges overlap\n";
+
+=head2 overlaps
+
+  Title   : overlaps
+  Usage   : if($r1->overlaps($r2)) { do stuff }
+  Function: tests if $r2 overlaps $r1
+  Args    : arg #1 = a range to compare this one to (mandatory)
+            arg #2 = optional strand-testing arg ('strong', 'weak', 'ignore')
+  Returns : true if the ranges overlap, false otherwise
+
+=cut
+
+=head2 contains
+
+  Title   : contains
+  Usage   : if($r1->contains($r2) { do stuff }
+  Function: tests whether $r1 totally contains $r2
+  Args    : arg #1 = a range to compare this one to (mandatory)
+                 alternatively, integer scalar to test
+            arg #2 = optional strand-testing arg ('strong', 'weak', 'ignore')
+  Returns : true if the argument is totally contained within this range
+
+=cut
+
+=head2 equals
+
+  Title   : equals
+  Usage   : if($r1->equals($r2))
+  Function: test whether $r1 has the same start, end, length as $r2
+  Args    : arg #1 = a range to compare this one to (mandatory)
+            arg #2 = optional strand-testing arg ('strong', 'weak', 'ignore')
+  Returns : true if they are describing the same range
+
+=cut
+
+=head1 Geometrical methods
+
+These methods do things to the geometry of ranges, and return
+Bio::RangeI compliant objects or triplets (start, stop, strand) from
+which new ranges could be built.
+
+=head2 intersection
+
+ Title   : intersection
+ Usage   : ($start, $stop, $strand) = $r1->intersection($r2); OR
+           ($start, $stop, $strand) = Bio::Range->intersection(\@ranges); OR
+           my $containing_range = $r1->intersection($r2); OR
+           my $containing_range = Bio::Range->intersection(\@ranges);
+ Function: gives the range that is contained by all ranges
+ Returns : undef if they do not overlap, or
+           the range that they do overlap (in the form of an object
+            like the calling one, OR a three element array)
+ Args    : arg #1 = [REQUIRED] a range to compare this one to,
+                    or an array ref of ranges
+           arg #2 = optional strand-testing arg ('strong', 'weak', 'ignore')
+
+=cut
+
+=head2 union
+
+   Title   : union
+    Usage   : ($start, $stop, $strand) = $r1->union($r2);
+            : ($start, $stop, $strand) = Bio::Range->union(@ranges);
+              my $newrange = Bio::Range->union(@ranges);
+    Function: finds the minimal Range that contains all of the Ranges
+    Args    : a Range or list of Range objects
+    Returns : the range containing all of the range
+              (in the form of an object like the calling one, OR
+              a three element array)
+
+=cut
+
+=head2 overlap_extent
+
+ Title   : overlap_extent
+ Usage   : ($a_unique,$common,$b_unique) = $a->overlap_extent($b)
+ Function: Provides actual amount of overlap between two different
+           ranges
+ Example :
+ Returns : array of values containing the length unique to the calling
+           range, the length common to both, and the length unique to
+           the argument range
+ Args    : a range
+
+=cut
+
+=head2 disconnected_ranges
+
+    Title   : disconnected_ranges
+    Usage   : my @disc_ranges = Bio::Range->disconnected_ranges(@ranges);
+    Function: finds the minimal set of ranges such that each input range
+              is fully contained by at least one output range, and none of
+              the output ranges overlap
+    Args    : a list of ranges
+    Returns : a list of objects of the same type as the input
+              (conforms to RangeI)
+
+=cut
+
+=head2 offsetStranded
+
+    Title    : offsetStranded
+    Usage    : $rnge->ofsetStranded($fiveprime_offset, $threeprime_offset)
+    Function : destructively modifies RangeI implementing object to
+               offset its start and stop coordinates by values $fiveprime_offset and
+               $threeprime_offset (positive values being in the strand direction).
+    Args     : two integer offsets: $fiveprime_offset and $threeprime_offset
+    Returns  : $self, offset accordingly.
+
+=cut
+
+=head2 subtract
+
+  Title   : subtract
+  Usage   : my @subtracted = $r1->subtract($r2)
+  Function: Subtract range r2 from range r1
+  Args    : arg #1 = a range to subtract from this one (mandatory)
+            arg #2 = strand option ('strong', 'weak', 'ignore') (optional)
+  Returns : undef if they do not overlap or r2 contains this RangeI,
+            or an arrayref of Range objects (this is an array since some
+            instances where the subtract range is enclosed within this range
+            will result in the creation of two new disjoint ranges)
+
+=cut
+
+1;
