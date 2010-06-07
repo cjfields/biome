4c5a4de20cb5c6939eec16b1f1137c1b372966c0
Chris Fields <cjfields@bioperl.org>
2010-06-07 18:05:37 -0500

    comment out error class stuff, broken in latest Moose 1.07

--- lib/Biome.pm
+++ lib/Biome.pm
@@ -11,18 +11,19 @@ use Biome::Meta::Class;
 # Note there is a bit of checking for Exception::Class here to make it optional.
 # This doesn't appear to be working as intended yet, though, and is a big TODO.
 
-our $EXCEPTION_CLASS = '';
+#our $EXCEPTION_CLASS = '';
 
 # to switch off Exception::Class usage if Exception::Class is installed,
 # set env. var. BP_EXCEPTION_CLASS = ''
-if (exists $ENV{BP_EXCEPTION_CLASS}) {
-	$EXCEPTION_CLASS = $ENV{BP_EXCEPTION_CLASS};
-} else {
-	eval {use Exception::Class; 1;};
-	if (!$@) {
-		$EXCEPTION_CLASS = 'Biome::Root::Error';
-	}
-}
+
+#if (exists $ENV{BP_EXCEPTION_CLASS}) {
+#	$EXCEPTION_CLASS = $ENV{BP_EXCEPTION_CLASS};
+#} else {
+#	eval {use Exception::Class; 1;};
+#	if (!$@) {
+#		$EXCEPTION_CLASS = 'Biome::Root::Error';
+#	}
+#}
 
 Moose::Exporter->setup_import_methods(also => 'Moose');
 
@@ -44,7 +45,8 @@ sub init_meta {
 	# Exception::Class::Base) which has methods lifted from the Moose tests.
 	# This'll need to be made more user-friendly at some point (maybe allow the
 	# Moose default?)
-	$moose->error_class($EXCEPTION_CLASS) if $EXCEPTION_CLASS;
+    
+	#$moose->error_class($EXCEPTION_CLASS) if $EXCEPTION_CLASS;
 	$moose;
 }
 
