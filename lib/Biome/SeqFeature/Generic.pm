b1ba37b93018023ef531cc837b64c187bcafeced
Chris Fields <cjfields@bioperl.org>
2012-02-19 21:05:32 -0600

    seqfeatures are just a location that may possibly have sublocations

--- lib/Biome/SeqFeature/Generic.pm
+++ lib/Biome/SeqFeature/Generic.pm
@@ -5,20 +5,13 @@ use namespace::clean -except => qw(meta);
 use List::MoreUtils qw(any);
 use Biome::Location::Simple;
 
+with 'Biome::Role::Location::Simple';
 with 'Biome::Role::Location::Split';
 with 'Biome::Role::Location::Locatable';
 with 'Biome::Role::SeqFeature';
-
-sub BUILD {
-    my ($self, $params) = @_;
-    if (any { exists $params->{$_} } qw(start end strand seq_id)) {
-        $self->add_sub_Location(Biome::Location::Simple->new(%$params));
-    }
-}
 
 __PACKAGE__->meta->make_immutable;
 
 1;
 
 __END__
-
