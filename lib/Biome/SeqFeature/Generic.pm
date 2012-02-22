3cf0de389221740a29205dffa17f57db214a7fbe
Chris Fields <cjfields@bioperl.org>
2012-02-21 21:51:13 -0600

    Move over to direct param. role

--- lib/Biome/SeqFeature/Generic.pm
+++ lib/Biome/SeqFeature/Generic.pm
@@ -6,7 +6,8 @@ use List::MoreUtils qw(any);
 use Biome::Location::Simple;
 
 with 'Biome::Role::Location::Simple';
-with 'Biome::Role::Location::Split';
+with 'Biome::Role::Location::Collection' => {base_name  => 'SeqFeature',
+                                             layered    => 0};
 with 'Biome::Role::Location::Locatable';
 with 'Biome::Role::SeqFeature';
 
